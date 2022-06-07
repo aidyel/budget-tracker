b723b06be4a2d70059319eafaff48359185b4131
aidyel <aidayeli@icloud.com>
2022-06-07 15:04:37 -0400

    updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ const APP_PREFIX = 'budget_tracker';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE = [
+const FILES_TO_CACHE =[
     './index.html',
     './css/style.css',
     './js/idb.js',
@@ -18,11 +18,28 @@ const FILES_TO_CACHE = [
     "./icons/icon-72x72.png",
 ]
 
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cahched, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache :' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+            console.log( 'installing cache :' + CACHE_NAME)
+            return cache.addAll
+            (FILES_TO_CACHE)
+    
         })
     )
 });
@@ -47,17 +64,3 @@ self.addEventListener('activate', function(e) {
     )
 })
 
-self.addEventListener('fetch', function(e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cahched, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
